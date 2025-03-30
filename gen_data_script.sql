feba430465ff616b2bce386fcd41b45ab1e3de4e
tuthon <tuthon@gmail.com>
2025-03-30 22:07:30 +0100

    Merged remote changes

--- gen_data_script.sql
+++ gen_data_script.sql
@@ -1,7 +1,7 @@
 DROP PROCEDURE IF EXISTS  `tools`.`gen_data_script`;
 
 delimiter $$
-CREATE DEFINER=`Sup_User_SPESA`@`%` PROCEDURE `tools`.`gen_data_script`(IN SchemeName VARCHAR(64), in tableName VARCHAR(64), in EngineName varchar(64))  ExitSub:BEGIN
+CREATE DEFINER=`Sup_User_SPESA`@`%` PROCEDURE `tools`.`gen_data_script`(IN SchemeName VARCHAR(64), in tableName VARCHAR(1000), in EngineName varchar(64))  ExitSub:BEGIN
 -- CALL tools.gen_data_script('SchemeName', tableName, EngineName);
 -- CALL tools.gen_data_script('', '', ''); -- All tables
 -- CALL tools.gen_data_script('SchemeName', '', ''); -- All tables for particular schema
@@ -10,7 +10,7 @@ CREATE DEFINER=`Sup_User_SPESA`@`%` PROCEDURE `tools`.`gen_data_script`(IN Schem
 -- CALL tools.gen_data_script('SchemeName', '', 'EngineName'); -- Only for particular engine and schema
 
 	DECLARE errNom INT DEFAULT 0;
-	DECLARE errText VARCHAR(255) DEFAULT '';
+	DECLARE errText, MysqlVer VARCHAR(255) DEFAULT '';
 
 	DECLARE EXIT HANDLER FOR SQLEXCEPTION
 		BEGIN
@@ -18,9 +18,11 @@ CREATE DEFINER=`Sup_User_SPESA`@`%` PROCEDURE `tools`.`gen_data_script`(IN Schem
 			SELECT errNom, errText;
 		END;
 
+SET MysqlVer = VERSION();
+
 SET SESSION group_concat_max_len = 50000;
 SELECT
-	CONCAT( 'mysqldump -P 3306 -hlocalhost -udba -pdba --no-create-db --no-create-info --column-statistics=0 --skip-triggers --extended-insert ',
+	CONCAT( 'mysqldump -P 3306 -hlocalhost -udba -pdba --no-create-db --no-create-info', IF(LEFT(MysqlVer, 2) = '5.', '', '--column-statistics=0'), ' --skip-triggers --extended-insert --default-character-set=utf8mb4 --no-tablespaces --databases ',
 	TABLE_SCHEMA, ' ', IF(tableName<>'', REPLACE ( GROUP_CONCAT( TABLE_NAME ), ',', ' ' ), ' ' ), ' > backup_', @@hostname, '_', TABLE_SCHEMA, '_data.sql' ) as data_gen_script
 FROM information_schema.`TABLES` 
 WHERE	`ENGINE` = IF(EngineName='', `ENGINE`, EngineName) 
